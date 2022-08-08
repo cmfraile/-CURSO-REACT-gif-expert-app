b574bef1db845258067911b8761a7b17f0611155
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-08 13:23:12 +0200

    75

--- src/componentes/main.tsx
+++ src/componentes/main.tsx
@@ -1,9 +1,26 @@
 import { useState } from 'react';
 import './main.sass';
+import { v4 } from 'uuid';
 
 export const main = () => {
 
-    const [ categorias , setCategorias ] = useState();
+    const [ categoriasHook , setCategorias ] = useState<string[]>([]); 
+    const addcategory = () => {
+        
+
+        /*
+        const boton:any = document.getElementById('input');
+        let arr:any = categoriasHook;
+        arr.push(boton.value);
+        setCategorias(arr);
+        boton.value = "" ;
+        */
+
+        const boton:any = document.getElementById('input');
+        setCategorias([...categoriasHook,boton.value]) ; boton.value = "";
+        
+
+    }
 
     return(
         <>
@@ -11,11 +28,15 @@ export const main = () => {
             <h1>Hola mundo</h1>
 
             {/*Input*/}
+
             <ol>
-                {['lorem','ipsum','dolor'].map(valor => {
-                    return <li key={valor}>{valor}</li>
+                {categoriasHook.map(valor => {
+                    return <li key={v4()}>{valor}</li>
                 })}
             </ol>
+
+            <input id='input' type="text"/>
+            <button onClick={addcategory} >Añadir</button>
 
             {/*Lista de gifs*/}
                 {/*Gif cards*/}
