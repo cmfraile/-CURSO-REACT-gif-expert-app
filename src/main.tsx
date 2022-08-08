bff3fd39e4ec768303efaaa8cb22209092713454
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-08 10:57:43 +0200

    importado bootstrap de la manera que sabemos

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './main.sass'
 
+//bootstrap:
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <h1>GifExpertApp</h1>
