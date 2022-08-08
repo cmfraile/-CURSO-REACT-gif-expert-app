f738f62646c2387a17236a0b885b109354d460d6
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-08 10:54:23 +0200

    terminamos la clase 72 pero vamos a tratar de instalar bootstrap

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './main.sass'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <h1>GifExpertApp</h1>
+  </React.StrictMode>
+)
