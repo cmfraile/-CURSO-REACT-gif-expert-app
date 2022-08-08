59f129f8045abf170025a70c8d6184d639ba2610
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-08 11:24:06 +0200

    el bootstrap solo instala el css

--- src/mainDOM.tsx
+++ src/mainDOM.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './main.sass'
+import './mainDOM.sass'
 
 //bootstrap:
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
