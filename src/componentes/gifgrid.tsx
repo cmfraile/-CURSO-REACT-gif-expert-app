aa03f71447a336157165fa95a61dc5c7cefc5bc5
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-09 15:56:28 +0200

    85

--- src/componentes/gifgrid.tsx
+++ src/componentes/gifgrid.tsx
@@ -1,53 +1,25 @@
-import { v4 } from "uuid";
-
-const placement = () => {
-    let caso:any[] = [];
-    for(let i = 1 ; i <= 5 ; i++){
-        caso.push(
-        <div    
-                key={v4()}
-                className="ggimg fondodefault"
-                style={{backgroundImage:'url(https://c.tenor.com/txPXESQJ_0AAAAAC/the-bad-guys-the-bad-guys-movie.gif)'}}
-        >
-            <div className="titulo">
-                Que ricos los papus
-            </div>
-        </div>
-        )
-    }
-    return caso;
-}
-
-const getGifs = async(categoria:string) => {
-
-    const querystring = ():string => {
-        const request:any = {
-            url:'https://api.giphy.com/v1/gifs/search?',
-            api_key:'wOW1tP8JYE60hILwEOnpZ4bIQwDPu7mg',
-            q:categoria,
-            limit:'5'
-        }
-        const caso:string[] = [request.url];
-        Object.keys(request).map(x => {
-            if(x == 'url'){return};
-            if(x == 'api_key'){caso.push(`${x}=${request[x]}`)}else{caso.push(`&${x}=${request[x]}`)}
-            
-        });
-        return caso.join('');
-    }
-    const { data } = await(await fetch(querystring())).json();
-    const gifs = data.map(({id,title,url}:any) => ({id,title,url}));
-    //console.log(gifs);
-}
+import { useEffect, useState } from "react";
+import { getGifs , placement } from '../helpers/getgifs';
 
 export const GifGrid = ({categoria}:{categoria:string}) => {
-    
-    getGifs(categoria);
+
+    const [images,setImages] = useState<{id:string,title:string,url:string}[]|[]>([]);
+    useEffect(() => {getGifs(categoria).then(setImages)},[]);
 
     return(
         <div className="gifgrid">
             <h3>{categoria}</h3>
-            {placement()}
+            {images.map(({id,title,url}) => {
+                {/*console.log(x)*/}
+            return(
+            <div    
+                key={id}
+                className="ggimg fondodefault"
+                style={{backgroundImage:`url(${url})`}}
+            >
+                <div className="titulo">{title}</div>
+            </div>
+            )})}
         </div>
     )
 }
---
+++ src/helpers/getgifs.tsx
@@ -0,0 +1,41 @@
+import { v4 } from "uuid";
+
+export const getGifs = async(categoria:string):Promise<{id:string,title:string,url:string}[]> => {
+
+    const querystring = () => {
+        const request:any = {
+            url:'https://api.giphy.com/v1/gifs/search?',
+            api_key:'wOW1tP8JYE60hILwEOnpZ4bIQwDPu7mg',
+            q:categoria,
+            limit:'5'
+        }
+        const caso:string[] = [request.url];
+        Object.keys(request).map(x => {
+            if(x == 'url'){return};
+            if(x == 'api_key'){caso.push(`${x}=${request[x]}`)}else{caso.push(`&${x}=${request[x]}`)}
+            
+        });
+        return caso.join('');
+    }
+    const { data } = await(await fetch(querystring())).json();
+    const gifs = data.map(({id,title,url}:any) => ({id,title,url}));
+    return gifs;
+}
+
+export const placement = () => {
+    let caso:any[] = [];
+    for(let i = 1 ; i <= 5 ; i++){
+        caso.push(
+        <div    
+                key={v4()}
+                className="ggimg fondodefault"
+                style={{backgroundImage:'url(https://c.tenor.com/txPXESQJ_0AAAAAC/the-bad-guys-the-bad-guys-movie.gif)'}}
+        >
+            <div className="titulo">
+                Que ricos los papus
+            </div>
+        </div>
+        )
+    }
+    return caso;
+}
