959c7ed223b2e8b73909654e80333224a9b1338e
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-09 13:01:45 +0200

    81

--- src/componentes/gifgrid.tsx
+++ src/componentes/gifgrid.tsx
@@ -1,21 +1,49 @@
-export const GifGrid = ({categoria}:any) => {
+import { v4 } from "uuid";
 
-    const placement = () => {
-        let caso:any[] = [];
-        for(let i = 1 ; i <= 5 ; i++){
-            caso.push(
-            <div    className="ggimg fondodefault"
-                    style={{backgroundImage:'url(https://c.tenor.com/txPXESQJ_0AAAAAC/the-bad-guys-the-bad-guys-movie.gif)'}}
-            >
-                <div className="titulo">
-                    Lorem, ipsum dolor.
-                </div>
+const placement = () => {
+    let caso:any[] = [];
+    for(let i = 1 ; i <= 5 ; i++){
+        caso.push(
+        <div    
+                key={v4()}
+                className="ggimg fondodefault"
+                style={{backgroundImage:'url(https://c.tenor.com/txPXESQJ_0AAAAAC/the-bad-guys-the-bad-guys-movie.gif)'}}
+        >
+            <div className="titulo">
+                Que ricos los papus
             </div>
-            )
-        }
-        return caso;
+        </div>
+        )
     }
+    return caso;
+}
+
+const getGifs = async(categoria:string) => {
+
+    const querystring = ():string => {
+        const request:any = {
+            url:'https://api.giphy.com/v1/gifs/search?',
+            api_key:'wOW1tP8JYE60hILwEOnpZ4bIQwDPu7mg',
+            q:categoria,
+            limit:'5'
+        }
+        const caso:string[] = [request.url];
+        Object.keys(request).map(x => {
+            if(x == 'url'){return};
+            if(x == 'api_key'){caso.push(`${x}=${request[x]}`)}else{caso.push(`&${x}=${request[x]}`)}
+            
+        });
+        return caso.join('');
+    }
+    const { data } = await(await fetch(querystring())).json();
+    const gifs = data.map(({id,title,url}:any) => ({id,title,url}));
+    //console.log(gifs);
+}
+
+export const GifGrid = ({categoria}:{categoria:string}) => {
     
+    getGifs(categoria);
+
     return(
         <div className="gifgrid">
             <h3>{categoria}</h3>
