9724b4b1ea4ae1f44421c9c1ec23d85e7079ce5c
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-10 15:05:40 +0200

    la solucion de añadir la linea de moduleNameMapper en el jestconfig es la que ha funcionado

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
     moduleNameMapper: {"^.+\\.(css|less|scss|sass)$": "babel-jest"},
     testEnvironment: 'jest-environment-jsdom',
-    setupFiles: ['./jest.setup.ts']
+    setupFiles: ['./jest.setup.ts'],
+    moduleNameMapper: {"^uuid$": require.resolve("uuid")}
     /*
     preset: 'ts-jest/presets/js-with-ts',
     
