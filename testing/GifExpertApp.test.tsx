1bab6290d80947426f3502f02b4f5ef2a6408a91
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-13 12:01:14 +0200

    puedes hacer mock y mandar de segundo argumento un callback vacio para importar el sass que da problemas

--- testing/GifExpertApp.test.tsx
+++ testing/GifExpertApp.test.tsx
@@ -1,6 +1,7 @@
 import { render , screen } from "@testing-library/react";
 import { main as GifExpertApp } from '../src/components/main';
 import React from "react";
+jest.mock('../src/components/main.sass',() => {})
 
 const main = () => {
     test('probamos toda la app',() => {
