06029bc45b7c6a6dcd54d5d560ba7e9adbeb12da
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-08-13 16:32:58 +0200

    vamos a la segunda app

--- testing/GifExpertApp.test.tsx
+++ testing/GifExpertApp.test.tsx
@@ -1,13 +1,54 @@
-import { render , screen } from "@testing-library/react";
+import { render , screen , fireEvent , waitFor, renderHook } from "@testing-library/react";
 import { main as GifExpertApp } from '../src/components/main';
 import React from "react";
+import { useFetchGifs } from "../src/hooks/useFetchGifs";
+
+
 jest.mock('../src/components/main.sass',() => {})
 
 const main = () => {
-    test('probamos toda la app',() => {
-        render(<GifExpertApp/>);
-        screen.debug();
+
+    const inputvalue:string = 'The bad guys'
+    
+    test('Screenshot',() => {
+        expect(render(<GifExpertApp/>).container).toMatchSnapshot();
     });
+
+    test('Debe de generar consulta si el input cumple los requisitos',() => {
+        render(<GifExpertApp/>);
+        const input:HTMLInputElement = screen.getByLabelText('inputcat');
+        const form:HTMLFormElement = screen.getByLabelText('form');
+        fireEvent.input(input,{target:{value:inputvalue}});
+        fireEvent.submit(form);
+        
+        expect(screen.getByRole('heading',{name:'Cargando...'})).toBeTruthy();
+        expect(input.value).toBe('');
+    
+    });
+
+    test('No debe de generar 2 consultas del mismo tema',() => {
+        render(<GifExpertApp/>)
+        const input:HTMLInputElement = screen.getByLabelText('inputcat');
+        const form:HTMLFormElement = screen.getByLabelText('form');
+        fireEvent.input(input,{target:{value:inputvalue}});
+        fireEvent.submit(form);
+        fireEvent.input(input,{target:{value:inputvalue}});
+        fireEvent.submit(form);
+
+        expect(screen.getAllByRole('heading',{name:'Cargando...'}).length).toBe(1);
+    
+    });
+
+    test('No debe de generar consulta si el input no cumple los requisitos',() => {
+        render(<GifExpertApp/>);
+        const input:HTMLInputElement = screen.getByLabelText('inputcat');
+        const form:HTMLFormElement = screen.getByLabelText('form');
+        fireEvent.input(input,{target:{value:'a'}});
+        expect(render(<GifExpertApp/>).container).toMatchSnapshot();
+        fireEvent.submit(form);
+        expect(render(<GifExpertApp/>).container).toMatchSnapshot();
+    })
+
 }
 
 describe('Prueba de la ruta crítica de GifExpertApp',main)
